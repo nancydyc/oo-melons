33be423f0873d779dab87157920ee754425efa62
Your Name <you@example.com>
2020-01-17 15:47:05 -0800

    completed Rush Hour and Splurge Pricing on melons.py

--- melons.py
+++ melons.py
@@ -1,4 +1,8 @@
 """Classes for melon orders."""
+
+from random import choice
+import datetime
+
 class AbstractMelonOrder():
     """An abstract base class that other Melon Orders inherit from."""
     def __init__(self, species, qty):
@@ -7,30 +11,46 @@ class AbstractMelonOrder():
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.fee = 0 
+        self.fee = 0
 
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
 
-        self.shipped = True     
+        self.shipped = True
+
+
+    def get_base_price(self):
+
+        self.base_price = choice(range(5, 10))
+        # self.base_price = 5
+
+        current_time = datetime.datetime.now()
+        current_day = current_time.weekday()
+        current_hour = current_time.hour
+
+        if current_day in range(1, 6) and current_hour in range(8, 12):
+        # if current_day in range(1, 6) and current_hour in range(8, 17):
+            self.base_price += 4
+
+
 
 
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        self.get_base_price()
 
         if self.species == 'Christmas melons':
-            base_price *= 1.5
+            self.base_price *= 1.5
 
         # total = (1 + self.tax) * self.qty * base_price
         
-        total = (1 + self.tax) * self.qty * base_price + self.fee
-        
+        total = (1 + self.tax) * self.qty * self.base_price + self.fee
 
-        return total
+        return round(total, 2)
    
+
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
@@ -68,10 +88,39 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
         return self.country_code
 
-order1 = DomesticMelonOrder('watermelon', 6)
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    tax = 0
+    passed_inspection = False
+
+    def mark_inspection(self, passed):
+        if passed:
+        # if passed:
+            self.passed_inspection = passed
+            # self.passed_inspection = True
+
+            # return self.passed_inspection
+        self.passed_inspection
+        # return self.passed_inspection
+
+order1 = DomesticMelonOrder('watermelon', 10)
 # print(order1.order_type, order1.tax)
+
 print(order1.get_total())
 
-order0 = InternationalMelonOrder("watermelon", 6, "AUS")
-print(order0.get_total())
-# print(order0.tax, order0.order_type, order0.country_code, order0.qty, order0.species)
+
+
+# order1 = DomesticMelonOrder('watermelon', 6)
+# print(order1.order_type, order1.tax)
+# print(order1.get_total())
+
+# order0 = InternationalMelonOrder("watermelon", 6, "AUS")
+# print(order0.get_total())
+# # print(order0.tax, order0.order_type, order0.country_code, order0.qty, order0.species)
+
+# order2 = GovernmentMelonOrder('watermelon', 6)
+# # print(order2.mark_inspection(False))
+# print(order2.passed_inspection)
+
+# # print(order2.mark_inspection(True))
+# print(order2.passed_inspection)
